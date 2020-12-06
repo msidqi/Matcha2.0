ec6cc8bd289f1d6caec58a60065bfa522d36943a
msidqi <mouadmr89@gmail.com>
2020-12-06 20:11:52 +0100

    chore: npm audit, update nextjs

--- components/ui/Navbar/index.tsx
+++ components/ui/Navbar/index.tsx
@@ -72,7 +72,7 @@ function Navbar() {
                         leaveTo="transform opacity-0 scale-95"
                     >
                     <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</a>
+                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Profile</a>
                         <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a>
                         <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign out</a>
                     </div>
