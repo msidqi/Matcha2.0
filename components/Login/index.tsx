16f4bb3f52a4a9b4a9a6eca1e434cbd8505fdd4c
msidqi <mouadmr89@gmail.com>
2020-12-06 20:41:03 +0100

    feat: initial login form

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -1,3 +1,19 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
 export const Login = (): JSX.Element => {
-  return <div className="m-auto">Hello World</div>;
+  const {register, handleSubmit} = useForm()
+
+  const onSubmit = (data: any) => console.log(data)
+  return <div className="m-auto">
+    <div className="bg-white border rounded">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label className="block text-gray-700 font-semibold	">Email</label>
+        <input name="email" ref={register} className="form-input block" />
+        <label className="block text-gray-700 font-semibold	">Password</label>
+        <input name="password" ref={register} type="password" className="form-input block" />
+        <input type="submit" />
+      </form>
+    </div>
+  </div>
 };
