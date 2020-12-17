829d3bcb6d11cc1a5fb386b6d4fd6bd1099d4ab5
msidqi <mouadmr89@gmail.com>
2020-12-17 21:00:39 +0100

    convert

--- components/ImageUpload/index.tsx
+++ components/ImageUpload/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import ImagePreview from './imagePreview'
+
+type ImagePreviewProps = {
+    title: string;
+    id: string;
+    src: string;
+    objectURL: string;
+    size: string;
+}
 
 const ImageUpload = () => {
     const [FILES, setFILES] = React.useState({})
-    const [counter, setCounter] = React.useState(0)
-    const [isHidden, setIsHidden] = React.useState(false)
+    // const [counter, setCounter] = React.useState(0)
+    const [showDropFileAction, setDropFileAction] = React.useState(false)
+    const [isEmpty, setIsEmpty] = React.useState(false)
     const [isDraggedover, setIsDraggedover] = React.useState(false)
+    const [imagePreviews, setImagePreviews] = React.useState<ImagePreviewProps[]>([])
 
     const fileTemplRef = React.createRef()
     const imageTemplRef = React.createRef()
@@ -13,40 +24,61 @@ const ImageUpload = () => {
         imageTempl = imageTemplRef.current,//document.getElementById("image-template"),
         empty = emptyRef.current;//document.getElementById("empty");
 
+    const handleImagePrevewDelete = (indexToDelete: number) => {
+        const elem = imagePreviews[indexToDelete]
+        console.log(elem)
+        if (elem) {
+            URL.revokeObjectURL(elem.objectURL)
+            setImagePreviews(imagePreviews.filter((_, i) => i !== indexToDelete))
+        }
+    }
     // check if file is of type image and prepend the initialied
     // template to the target element
     function addFile(target, file) {
         const isImage = file.type.match("image.*"),
             objectURL = URL.createObjectURL(file);
+        if (!isImage) console.error('not an image')
+        console.log('objectURL', objectURL)
 
-        const clone = isImage
-            ? imageTempl.content.cloneNode(true)
-            : fileTempl.content.cloneNode(true);
-
-        clone.querySelector("h1").textContent = file.name;
-        clone.querySelector("li").id = objectURL;
-        clone.querySelector(".delete").dataset.target = objectURL;
-        clone.querySelector(".size").textContent =
-            file.size > 1024
+        // const clone = imageTempl.content.cloneNode(true);
+        const cloneProps: ImagePreviewProps = {
+            title: file.name,
+            id: objectURL,
+            objectURL: objectURL,
+            src: objectURL,
+            size: file.size > 1024
                 ? file.size > 1048576
                     ? Math.round(file.size / 1048576) + "mb"
                     : Math.round(file.size / 1024) + "kb"
-                : file.size + "b";
+                : file.size + "b"
+        }
+        // clone.querySelector("h1").textContent = file.name;
+        // clone.querySelector("li").id = objectURL;
+        // clone.querySelector(".delete").dataset.target = objectURL;
+        // clone.querySelector(".size").textContent =
+        // file.size > 1024
+        //     ? file.size > 1048576
+        //         ? Math.round(file.size / 1048576) + "mb"
+        //         : Math.round(file.size / 1024) + "kb"
+        //     : file.size + "b";
 
-        isImage &&
-            Object.assign(clone.querySelector("img"), {
-                src: objectURL,
-                alt: file.name
-            });
+        if (isImage) {
+            console.log('render preview with props =>', cloneProps)
+        }
+        // isImage &&
+        //     Object.assign(clone.querySelector("img"), {
+        //         src: objectURL,
+        //         alt: file.name
+        //     });
 
         // empty.classList.add("hidden");
-        setIsHidden(true)
-        target.prepend(clone);
+        setIsEmpty(true)
+        // target.prepend(clone);
+        setImagePreviews([...imagePreviews, cloneProps])
 
-        FILES[objectURL] = file;
-        setFILES({ ...FILES })
+        setFILES({ ...FILES, objectURL: file })
     }
-
+    console.log('FILES', FILES)
     const galleryRef = React.createRef()
     const overlayRef = React.createRef()
     const gallery = galleryRef.current, //document.getElementById("gallery"),
@@ -64,8 +96,9 @@ const ImageUpload = () => {
     };
 
     // use to check if a file is being dragged
-    const hasFiles = ({ dataTransfer: { types = [] } }) =>
-        types.indexOf("Files") > -1;
+    // const hasFiles = ({ dataTransfer: { types = [] } }) =>
+    //     types.indexOf("Files") > -1;
+    const hasFiles = () => imagePreviews.length > 0;
 
 
 
@@ -74,9 +107,8 @@ const ImageUpload = () => {
         ev.preventDefault();
         for (const file of ev.dataTransfer.files) {
             addFile(gallery, file);
-            // overlay.classList.remove("draggedover");
-            setIsDraggedover(false)
-            setCounter(0);
+            // setIsDraggedover(false)
+            // setCounter(0);
         }
     }
 
@@ -86,35 +118,35 @@ const ImageUpload = () => {
         if (!hasFiles(e)) {
             return;
         }
-        setCounter((prevCounter) => prevCounter + 1);
-        if (counter + 1)
-            setIsDraggedover(true)
-        // overlay.classList.add("draggedover");
+        console.log('hasFiles', imagePreviews.length > 0)
+        // setCounter((prevCounter) => prevCounter + 1);
+        // if (counter + 1)
+        //     setIsDraggedover(true)
     }
 
     function dragLeaveHandler(e) {
-        setCounter((prevCounter) => prevCounter - 1)
-        if (1 > counter - 1)
-            // overlay.classList.remove("draggedover");
-            setIsDraggedover(false)
+        // setCounter((prevCounter) => prevCounter - 1)
+        // if (1 > counter - 1)
+        //     setIsDraggedover(false)
+        // console.log('counter', counter)
     }
 
     function dragOverHandler(e) {
-        if (hasFiles(e)) {
-            e.preventDefault();
-        }
+        // if (hasFiles(e)) {
+        e.preventDefault();
+        // }
     }
 
     // event delegation to caputre delete events
     // fron the waste buckets in the file preview cards
     const onGalleryClick = ({ target }) => {
-        if (target.classList.contains("delete")) {
-            const ou = target.dataset.target;
-            document.getElementById(ou).remove(ou);
-            gallery.children.length === 1 && setIsHidden(false)//empty.classList.remove("hidden");
-            delete FILES[ou];
-            setFILES({ ...FILES })
-        }
+        // if (target.classList.contains("delete")) {
+        //     const ou = target.dataset.target;
+        //     document.getElementById(ou).remove(ou);
+        //     gallery.children.length === 1 && setIsEmpty(false)//empty.classList.remove("hidden");
+        //     delete FILES[ou];
+        //     setFILES({ ...FILES })
+        // }
     };
 
     // print all selected files
@@ -125,13 +157,14 @@ const ImageUpload = () => {
 
     // clear entire selection
     const clearSelection = () => {
-        while (gallery.children.length > 0) {
-            gallery.lastChild.remove();
-        }
-        setFILES({})
+        // while (gallery.children.length > 0) {
+        //     gallery.lastChild.remove();
+        // }
+        // setFILES({})
+        setImagePreviews([])
         // empty.classList.remove("hidden");
-        setIsHidden(false)
-        gallery.append(empty);
+        setIsEmpty(false)
+        // gallery.append(empty);
     };
 
     return (
@@ -139,9 +172,9 @@ const ImageUpload = () => {
             <div className="bg-gray-500 h-screen w-screen sm:px-8 md:px-16 sm:py-8">
                 <main className="container mx-auto max-w-screen-lg h-full">
                     {/* file upload modal */}
-                    <article aria-label="File Upload Modal" className="relative h-full flex flex-col bg-white shadow-xl rounded-md" onDrop={dropHandler} onDragOver={dragOverHandler} onDragLeave={dragLeaveHandler} onDragEnter={dragEnterHandler}>
+                    <article aria-label="File Upload Modal" className="relative h-full flex flex-col bg-white shadow-xl rounded-none sm:rounded-md" onDrop={dropHandler} onDragOver={dragOverHandler} onDragLeave={dragLeaveHandler} onDragEnter={dragEnterHandler}>
                         {/* overlay */}
-                        <div id="overlay" ref={overlayRef} className={`w-full h-full absolute top-0 left-0 pointer-events-none z-50 flex flex-col items-center justify-center rounded-md ${isDraggedover && 'draggedover'}`}>
+                        <div id="overlay" ref={overlayRef} className={`w-full h-full absolute top-0 left-0 pointer-events-none z-50 flex flex-col items-center justify-center rounded-md ${isDraggedover && ' bg-gray-100 bg-opacity-50'}`}>
                             <i>
                                 <svg className="fill-current w-12 h-12 mb-3 text-blue-700" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                                     <path d="M19.479 10.092c-.212-3.951-3.473-7.092-7.479-7.092-4.005 0-7.267 3.141-7.479 7.092-2.57.463-4.521 2.706-4.521 5.408 0 3.037 2.463 5.5 5.5 5.5h13c3.037 0 5.5-2.463 5.5-5.5 0-2.702-1.951-4.945-4.521-5.408zm-7.479-1.092l4 4h-3v4h-2v-4h-3l4-4z" />
@@ -159,15 +192,18 @@ const ImageUpload = () => {
                                 <input id="hidden-input" onChange={onHiddenChange} ref={hiddenRef} type="file" multiple className="hidden" />
                                 <button id="button" onClick={onButtonClick} className="mt-2 rounded-sm px-3 py-1 bg-gray-200 hover:bg-gray-300 focus:shadow-outline focus:outline-none">
                                     Upload a file
-              </button>
+                                </button>
                             </header>
 
                             <h1 className="pt-8 pb-3 font-semibold sm:text-lg text-gray-900">
                                 To Upload
-            </h1>
+                            </h1>
 
-                            <ul id="gallery" ref={galleryRef} onClick={onGalleryClick} className="flex flex-1 flex-wrap -m-1">
-                                <li id="empty" ref={emptyRef} className={`h-full w-full text-center flex flex-col justify-center items-center ${isHidden && 'hidden'}`}>
+                            <ul id="gallery" onClick={onGalleryClick} className="flex flex-1 flex-wrap -m-1">
+                                {imagePreviews.map((elem, index) => (
+                                    <ImagePreview key={index} {...elem} onClickDelete={() => handleImagePrevewDelete(index)} />
+                                ))}
+                                <li id="empty" className={`h-full w-full text-center flex flex-col justify-center items-center ${isEmpty && 'hidden'}`}>
                                     <img className="mx-auto w-32" src="https://user-images.githubusercontent.com/507615/54591670-ac0a0180-4a65-11e9-846c-e55ffce0fe7b.png" alt="no data" />
                                     <span className="text-small text-gray-500">No files selected</span>
                                 </li>
@@ -178,44 +214,15 @@ const ImageUpload = () => {
                         <footer className="flex justify-end px-8 pb-8 pt-4">
                             <button id="submit" onClick={handleSubmit} className="rounded-sm px-3 py-1 bg-blue-700 hover:bg-blue-500 text-white focus:shadow-outline focus:outline-none">
                                 Upload now
-            </button>
+                            </button>
                             <button id="cancel" onClick={clearSelection} className="ml-3 rounded-sm px-3 py-1 hover:bg-gray-300 focus:shadow-outline focus:outline-none">
                                 Cancel
-            </button>
+                            </button>
                         </footer>
                     </article>
                 </main>
             </div>
-
-            <template id="file-template" ref={fileTempl}>
-                { /*<!--using two similar templates for simplicity in js code-- >*/}
-                <li className="block p-1 w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 xl:w-1/8 h-24">
-                    <article tabIndex={0} className="group w-full h-full rounded-md focus:outline-none focus:shadow-outline elative bg-gray-100 cursor-pointer relative shadow-sm">
-                        <img alt="upload preview" className="img-preview hidden w-full h-full sticky object-cover rounded-md bg-fixed" />
-
-                        <section className="flex flex-col rounded-md text-xs break-words w-full h-full z-20 absolute top-0 py-2 px-3">
-                            <h1 className="flex-1 group-hover:text-blue-800"></h1>
-                            <div className="flex">
-                                <span className="p-1 text-blue-800">
-                                    <i>
-                                        <svg className="fill-current w-4 h-4 ml-auto pt-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                            <path d="M15 2v5h5v15h-16v-20h11zm1-2h-14v24h20v-18l-6-6z" />
-                                        </svg>
-                                    </i>
-                                </span>
-                                <p className="p-1 size text-xs text-gray-700"></p>
-                                <button className="delete ml-auto focus:outline-none hover:bg-gray-300 p-1 rounded-md text-gray-800">
-                                    <svg className="pointer-events-none fill-current w-4 h-4 ml-auto" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                        <path className="pointer-events-none" d="M3 6l3 18h12l3-18h-18zm19-4v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.316c0 .901.73 2 1.631 2h5.711z" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </section>
-                    </article>
-                </li>
-            </template>
-
-            <template id="image-template" ref={imageTemplRef}>
+            {/* <template id="image-template" ref={imageTemplRef}>
                 <li className="block p-1 w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 xl:w-1/8 h-24">
                     <article tabIndex={0} className="group hasImage w-full h-full rounded-md focus:outline-none focus:shadow-outline bg-gray-100 cursor-pointer relative text-transparent hover:text-white shadow-sm">
                         <img alt="upload preview" className="img-preview w-full h-full sticky object-cover rounded-md bg-fixed" />
@@ -241,7 +248,7 @@ const ImageUpload = () => {
                         </section>
                     </article>
                 </li>
-            </template>
+            </template> */}
         </>
     )
 }
