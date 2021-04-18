01b5673e3fef75d6430767636baa00e0961d94f7
msidqi <mouadmr89@gmail.com>
2021-04-18 17:22:51 +0000

    feat: add simple carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,27 @@
 import Layout from "@/components/ui/Layout";
-// import Tag from "@/components/Tag";
+import Carousel from "@/modules/Carousel/components/Carousel";
+import ArrowNext from "@/components/ui/Icons/ArrowNext";
+import ArrowPrev from "@/components/ui/Icons/ArrowPrev";
 
 const index = (): JSX.Element => {
-  return <Layout>Index Page</Layout>;
+  return (
+    <Layout>
+      <Carousel
+        containerClassName="h-44 bg-red-50 w-96"
+        prevArrow={
+          <button className="cursor-pointer">
+            <ArrowPrev />
+          </button>
+        }
+        nextArrow={
+          <button className="cursor-pointer">
+            <ArrowNext />
+          </button>
+        }
+        items={[<div>hello</div>, <div>world</div>, <div>test</div>]}
+      />
+    </Layout>
+  );
 };
 
 export default index;
