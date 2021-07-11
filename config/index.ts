c8ffc4ac13fdb13855070ba5e74113af5b9196a7
msidqi <mouadmr89@gmail.com>
2021-07-11 01:58:07 +0100

    fix: socket server env var

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 const config = {
   IP_API_TOKEN: process.env.NEXT_PUBLIC_IP_API_TOKEN || "",
   API_URL: process.env.API_URL || "http://localhost:5000",
-  SOCKET_SERVER_PORT: process.env.SOCKET_SERVER_PORT || 5000,
+  SOCKET_SERVER_PORT: process.env.NEXT_PUBLIC_SOCKET_SERVER_PORT || 5000,
   MAPBOX_KEY: process.env.NEXT_PUBLIC_MAPBOX_KEY || "",
 };
 
